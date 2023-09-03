a8e9855b6280c0f9e8f6e04481a359e964e4eae6
Manikandan Thiyagarajan <67586339+tmanikandanmca@users.noreply.github.com>
2023-09-03 13:01:48 +0530

    Add project files.

---
+++ DotNetBasic/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetBasic;
+
+Console.WriteLine("Hello, World!");
+int x = 25, y = 5;
+int addition =0, subtraction;
+ValuesvsReference.AddData(x, y,ref addition,out subtraction);
+
+
---
+++ DotNetBasic/ValuesvsReference.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBasic;
+
+public class ValuesvsReference
+{
+
+    //Difference1: Updating the Ref and Out variables Inside the Method
+    public static void AddData(int x, int y, ref int addition
+       , out int subtraction)
+    {
+        subtraction = 0;
+    }
+    //Difference1:  Initializing the Ref and Out variables while passing to the Method
+    public static void AddData1(int x, int y, ref int addition
+       , out int subtraction)
+    {
+
+
+    }
+
+
+}
+
+
+
+
+public static class RefClass
+{
+
+    //Difference1: Updating the Ref and Out variables Inside the Method
+    public static void AddData(int x, int y, ref int addition
+       , out int subtraction)
+    {
+        subtraction = 0;
+    }
+    //Difference1:  Initializing the Ref and Out variables while passing to the Method
+    public static void AddData1(int x, int y, ref int addition
+       , out int subtraction)
+    {
+
+
+    }
+
+    public static void basicOperation(int x, int y, ref int addition
+        , ref int subtraction, ref int multiplication
+        , ref int dividion, ref int modules)
+    {
+
+
+    }
+}
+
+
+public class OutClass
+{
+
+    public void basicOperation(int x, int y, out int addition
+        , out int subtraction, out int multiplication
+        , out int dividion, out int modules)
+    {
+        addition = x + y;
+        subtraction = x - y;
+        multiplication = x * y;
+        dividion = x / y;
+        modules = x % y;
+
+    }
+}
